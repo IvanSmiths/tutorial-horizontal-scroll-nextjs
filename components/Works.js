e18fdd65bedac89ce51b13c4740e193b0eef71f0
IvanSmiths <info@ivansmiths.com>
2022-12-14 17:22:50 +0100

    finish structure works

--- components/Works.js
+++ components/Works.js
@@ -8,8 +8,16 @@ function Works() {
         <div className="work__copy">
           <h2>Lorem ipsum dolor sit.</h2>
         </div>
-        <div className="work__content">
-          <img src="/work.jpg" alt="work" />
+        <div className="work__content-outer">
+          <div className="work__content-inner">
+            <img src="/work.jpg" alt="work" />
+            <span className="work__line"></span>
+            <div className="work__details">
+              <h3>Next.js</h3>
+              <h3>2022</h3>
+              <h3>Employee</h3>
+            </div>
+          </div>
         </div>
       </div>
     </section>
