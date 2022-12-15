4c56abba3b7ca78de147d619fe826e2aec346bf6
IvanSmiths <info@ivansmiths.com>
2022-12-15 11:22:05 +0100

    add structure hrznt scroll

--- components/Works.js
+++ components/Works.js
@@ -1,21 +1,52 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 function Works() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const scrollRef = useRef(null);
+  const triggerRef = useRef(null);
+
+  useEffect(() => {
+    const pin = gsap.fromTo(
+      scrollRef.current,
+      {
+        translateX: 0,
+      },
+      {
+        ease: "none",
+        duration: 0.5,
+        scrollTrigger: {
+          trigger: triggerRef.current,
+          start: "top top",
+          end: "6000 top",
+          scrub: 0.6,
+          pin: true,
+        },
+      }
+    );
+    return () => {
+      pin.kill();
+    };
+  }, []);
   return (
-    <section className="works">
-      <div className="work">
-        <div className="work__copy">
-          <h2>Lorem ipsum dolor sit.</h2>
-        </div>
-        <div className="work__content-outer">
-          <div className="work__content-inner">
-            <img src="/work.jpg" alt="work" />
-            <span className="work__line"></span>
-            <div className="work__details">
-              <h3>Next.js</h3>
-              <h3>2022</h3>
-              <h3>Employee</h3>
+    <section ref={triggerRef}>
+      <div ref={scrollRef} className="works">
+        <div className="work">
+          <div className="work__copy">
+            <h2>Lorem ipsum dolor sit.</h2>
+          </div>
+          <div className="work__content-outer">
+            <div className="work__content-inner">
+              <img src="/work.jpg" alt="work" />
+              <span className="work__line"></span>
+              <div className="work__details">
+                <h3>Next.js</h3>
+                <h3>2022</h3>
+                <h3>Employee</h3>
+              </div>
             </div>
           </div>
         </div>
