baedac62ca42eabe83f70a4f12ba1b3e3f31caa5
IvanSmiths <info@ivansmiths.com>
2022-12-16 22:41:14 +0100

    add basic structure

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Hero from "../components/Hero";
 import Works from "../components/Works";
+import Basic from "../components/Basic";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Works />
+      <Basic />
     </>
   );
 }
