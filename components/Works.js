e2721619f9b8bf81f69fbb0f2a7108caf8e928bb
IvanSmiths <info@ivansmiths.com>
2022-12-16 12:18:36 +0100

    add scroll

--- components/Works.js
+++ components/Works.js
@@ -16,6 +16,7 @@ function Works() {
         translateX: 0,
       },
       {
+        translateX: "-100vw",
         ease: "none",
         duration: 0.5,
         scrollTrigger: {
@@ -34,6 +35,22 @@ function Works() {
   return (
     <section ref={triggerRef}>
       <div ref={scrollRef} className="works">
+        <div className="work">
+          <div className="work__copy">
+            <h2>Lorem ipsum dolor sit.</h2>
+          </div>
+          <div className="work__content-outer">
+            <div className="work__content-inner">
+              <img src="/work.jpg" alt="work" />
+              <span className="work__line"></span>
+              <div className="work__details">
+                <h3>Next.js</h3>
+                <h3>2022</h3>
+                <h3>Employee</h3>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="work">
           <div className="work__copy">
             <h2>Lorem ipsum dolor sit.</h2>
