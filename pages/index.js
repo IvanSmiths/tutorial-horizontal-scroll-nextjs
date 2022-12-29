e13f4f8eb66b1ea7fed379ef8494f0e1a84f492a
IvanSmiths <info@ivansmiths.com>
2022-12-29 16:43:38 +0100

    add complex version

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Hero from "../components/Hero";
 import Works from "../components/Works";
 import Basic from "../components/Basic";
+import Complex from "../components/Complex";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Basic />
+      <Complex />
     </>
   );
 }
